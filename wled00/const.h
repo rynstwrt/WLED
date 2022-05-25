1a8321e937f60518158f80611907251ca2b48ece
Ryan Stewart <ryanstewartalex@gmail.com>
2022-05-25 16:25:32 -0500

    Disable WiFi

--- wled00/const.h
+++ wled00/const.h
@@ -6,9 +6,9 @@
  */
 
 //Defaults
-#define DEFAULT_CLIENT_SSID "Your_Network"
-#define DEFAULT_AP_PASS     "wled1234"
-#define DEFAULT_OTA_PASS    "wledota"
+#define DEFAULT_CLIENT_SSID ""
+#define DEFAULT_AP_PASS     ""
+#define DEFAULT_OTA_PASS    ""
 
 //increase if you need more
 #ifndef WLED_MAX_USERMODS
